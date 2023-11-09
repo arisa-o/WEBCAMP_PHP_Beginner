75947d39a7ef5678586ce9e1582ccd27582fb932
arisa <ars.1996.jump@gmail.com>
2023-11-09 16:05:51 +0000

    型関連

---
+++ type_string.php
@@ -0,0 +1,14 @@
+<?php
+$s = "Hello";
+var_dump($s);
+
+echo $s[1] , "\n";
+
+$multi_string = "あいうえお";
+var_dump($multi_string);
+echo $multi_string[2] , "\n";
+
+$num_i =123;
+$num_s ="123";
+var_dump($num_i);
+var_dump($num_s);
